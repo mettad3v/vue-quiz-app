fd278b7ec7d9ee577be581c88c542b8817143de4
Emmanuel Syke <65050715+milestoneTech-io@users.noreply.github.com>
2020-06-16 01:21:27 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <Header 
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+    <b-container>
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox v-if="questions.length"
+            :currentQuestion = "questions[index]" 
+            :next = "next"
+            :increment = "increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div> 
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next (){
+      this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect){
+        this.numCorrect++
+      }
+      this.numTotal++
+    }
+  }, 
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results
+    })
+  }
+
+}
+</script>
+
+<style scoped>
+#app{
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50 ;
+  margin-top: 60px;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: greyscale;
+}
+</style>
